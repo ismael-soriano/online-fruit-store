83eb54183df60aabcf033d6969a057468561a803
ismael-soriano <luzaoban@gmail.com>
2016-05-06 14:34:31 +0200

    Last changes

---
+++ OnlineFruitShop/Controllers/ShopController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Tickets;
+using Infrastructure;
+
+namespace OnlineFruitShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        //
+        // GET: /Ticket/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
